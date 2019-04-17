b2ca90162e68ddd8d9f62c68e34c04da74038a54
ashleychari1234 <ashley.c.francisco.21@dartmouth.edu>
2019-04-17 17:28:34 -0400

    done with workshop

--- src/youtube-api.js
+++ src/youtube-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://www.googleapis.com/youtube/v3/search';
-const API_KEY = 'YOUR API KEY'; // add your own key here
+const API_KEY = 'AIzaSyCA_phXyMQSD7bieHvXEv9EZvMDzdLf_NQ'; // add your own key here
 
 const youtubeSearch = (term) => {
   const params = {
